0272b837ce86a2b3993369e969b03c05ef4b987e
Pranayy1 <pranayy7024@gmail.com>
2025-10-16 18:00:13 +0530

    Set production as default NODE_ENV

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || 'AIzaSyAweoatxBo6mI4pUccJ33crj3ZpghZZKog',
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   PORT: process.env.PORT || 5000
 };
